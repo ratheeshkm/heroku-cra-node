aa2a057e8532cc5f4f73e00e95ec938ce27d41aa
Ratheesh <ratkumar@linkedin.com>
2020-12-16 03:40:01 +0530

    Final test

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -58,7 +58,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React RKM
+          Learn React Test
         </a></p>
       </header>
     </div>
