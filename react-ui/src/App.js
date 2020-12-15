b8c666d517853fbab6c8d77cce4981a4d9ed4cca
Ratheesh <ratkumar@linkedin.com>
2020-12-16 02:18:55 +0530

    Test commit

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -58,7 +58,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React RKM
         </a></p>
       </header>
     </div>
